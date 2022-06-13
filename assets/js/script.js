587ee3b48497cb5cec50abf07f6c2fb0bd0a3cf5
charlie <gn4rtistic@gmail.com>
2022-06-13 16:23:48 -0700

    js file final draft

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var searchInputEl = $("#searchInput");
 var searchFormEl = $("#searchForm");
-var coinDataContainer = $("#coinContainer");
+var coinDataContainer = $("#coinDataContainer");
 var searchHistoryEl = $("#previousSearches");
 var coinPriceEl = $("#coinPrice");
 var searchHistoryContainer = $("#searcHistoryContainer")
@@ -17,13 +17,11 @@ function coinsListapi(coinName) { // ref line 19
         .then(function (response) {
             response.json()
                 .then(function (data) {
-                    console.log(data);
                     coinName = data.name; // ex: {"name: 'Bitcoin'"} the name
                     coinId = data.id; // ex: {"id: 'bitcoin'"} the ID, this one matters because if lowercase, the fetch url below will not accept it as a parameter. 
                     coinIcon = data.image.small; // ex: {"image: 'small'"} the icon
                     coinSymbol = data.symbol; // ex: {"symbol: 'btc'"} this is the coin's ticker
                     coinDescription = data.description.en; // ex {"description: en: (some text)"} the description
-                    console.log(coinPrice);
                     // setting parameters for the following functions
                     displayCurrentData(coinName, coinIcon, coinSymbol, coinDescription);
                     PriceData(coinId);
@@ -38,15 +36,14 @@ function PriceData(coinId) {
         .then(function (response) {
             response.json()
                 .then(function (data) {
-                    console.log(data);
                     coinPrice = data[0].current_price;
                     coinName = data.name;
-                    console.log(coinPrice);
                     displayCurrentData(coinName, coinIcon, coinSymbol, data[0].current_price, coinDescription)
                 })
         })
 }
 function displayCurrentData(coinName, coinIcon, coinSymbol, coinPrice, coinDescription) {
+    $("#coinDataContainer").attr("class", "card cr col-8 s5 hoverable");
     $("#coinName").html(coinName);// coin name
     $("#coinSymbol").html(coinSymbol); // coin symbol
     $("#icon").attr("src", `${coinIcon}`); //icon
@@ -61,6 +58,7 @@ function displayCurrentData(coinName, coinIcon, coinSymbol, coinPrice, coinDescr
 
     $("#coinPrice").html(coinPriceDisp);// coin price
 }
+
 
 function start() {
     searchFormEl.submit(searchInput)
